0851baa22d463401d43ba8c76f4fb54daee755a6
lucyleeow <jliu176@gmail.com>
2018-11-02 10:54:20 +1100

    add extra option filter_NA

--- R/filter.R
+++ R/filter.R
@@ -7,11 +7,18 @@
 #'     filtered.
 #'
 #' @param df Dataframe of Cell Profiler data.
+#' @param NA_cutoff 
 #' 
+#' @importFrom assertthat assert_that
 #'
 #' @describeIn filter_NA
 #' @export
-filter_NA <- function(df){
+filter_NA <- function(df, NA_cutoff){
+
+  # check inputs
+  assert_that(is.numeric(NA_cutoff), length(NA_cutoff) == 1,
+              msg = "Check that 'NA_cutoff' is a single number")
+  
   
   # calculate total NAs for each column
   col_NAs <- apply(df, 2, function(x) sum(is.na(x)))
@@ -20,12 +27,23 @@ filter_NA <- function(df){
     
     print("There were no columns with >0 NA's")
     
-  } else {
+  } 
+  
+  if (missing(NA_cutoff)){
     
-    print(paste("There were ", sum(col_NAs > 0), " columns with >0 NA's and will all be removed"))
+    print(paste("There were ", sum(col_NAs > 0), " columns with >0 NA's and all will be removed"))
     
     # filter columns
     df <- df[,col_NAs == 0]
+    
+  } else {
+    
+  filter_num <- sum(col_NAs < NA_cutoff)
+
+  print(paste("There were ", filter_num, " columns with >", NA_cutoff, " NAs and these will be removed", sep = ""))
+  
+  # filter columns
+  df <- df[,col_NAs > NA_cutoff]
     
   }
   
