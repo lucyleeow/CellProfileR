43dd525b23ebba7cf89d91d1e942b6a5f346f82d
lucyleeow <jliu176@gmail.com>
2018-11-02 15:09:13 +1100

    coding typos

--- R/filter.R
+++ R/filter.R
@@ -16,36 +16,34 @@
 filter_NA <- function(df, NA_cutoff){
 
   # check inputs
+  if (! missing(NA_cutoff)) {
   assert_that(is.numeric(NA_cutoff), length(NA_cutoff) == 1,
               msg = "Check that 'NA_cutoff' is a single number")
-  
+  }
   
   # calculate total NAs for each column
   col_NAs <- apply(df, 2, function(x) sum(is.na(x)))
   
-  if (sum(col_NAs > 0)) {
+  if (sum(col_NAs >= 1) == 0) {
     
     print("There were no columns with >0 NA's")
     
-  } 
-  
-  if (missing(NA_cutoff)){
+  } else if (missing(NA_cutoff)){
     
-    filter_cols <- colnames(df)[col_NAs < NA_cutoff]
-    cat("The columns:\n", filter_cols, "will be removed")
+    filter_cols <- colnames(df)[col_NAs >= 1]
+    cat("The columns:\n", filter_cols, "were removed")
     
-
-    # filter columns
+    # keep columns with 0 NA values only
     df <- df[,col_NAs == 0]
-    
+
   } else {
     
-  filter_cols <- colnames(df)[col_NAs < NA_cutoff]
-  cat("The columns:\n", filter_cols, "will be removed")
+  filter_cols <- colnames(df)[col_NAs > NA_cutoff]
+  cat("The columns:\n", filter_cols, "were removed")
   
   
   # filter columns
-  df <- df[,col_NAs > NA_cutoff]
+  df <- df[,col_NAs <= NA_cutoff]
     
   }
   
@@ -55,11 +53,28 @@ filter_NA <- function(df, NA_cutoff){
 
 #' @describeIn filter_NA
 #' @export
-filter_lowVar <- function(df){
+filter_lowVar <- function(df, freqCut = 95/5, uniqueCut = 10){
   
+  to_filter <- caret::nearZeroVar(df, freqCut, uniqueCut)
+  
+  if (length(to_filter) == 0) {
+    
+    cat("There were no columns with near zero variance")
+    
+  } else {
+    
+    filter_cols <- colnames(df)[to_filter]
+    cat("The columns:\n", filter_cols, "were removed")
+    
+    # filter df
+    df <- df[,-filter_cols]
+    
+  }
+  
+  return(df)
   
 }
 
 
-c <- c("ff","ffff","f3f")
-message("there",c)
+
+
