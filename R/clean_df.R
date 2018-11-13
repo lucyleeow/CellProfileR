9f1bc66d8cd740a47406fdc3c137884fb5ba9a0c
lucyleeow <jliu176@gmail.com>
2018-11-13 17:38:59 +1100

    Add documented fun

---
+++ R/clean_df.R
@@ -0,0 +1,27 @@
+#' Clean raw data
+#' 
+#' Remove redundant columns from the raw Cell Profiler data.
+#' 
+#' @return A dataframe with only the necessary plate, well, count, median and
+#'     image quality columns.
+#'     
+#' @param df Dataframe of raw Cell Profiler data.
+#'
+#' @importFrom magrittr %>%   
+#'   
+
+#' @export
+clean_df <- function(df) {
+  
+  df_clean <- df %>% 
+    dplyr::select(Metadata_Barcode, 
+           Metadata_WellID,
+           ImageNumber,
+           Count_Cells,
+           Count_Cells_unfiltered,
+           starts_with("Median"),
+           starts_with("ImageQuality_PowerLogLogSlope_Orig"))
+  
+  return(df_clean)
+  
+}
