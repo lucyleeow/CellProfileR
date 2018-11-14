c13acf12ac90d464934b8bd875fb8f399d0e85c4
lucyleeow <jliu176@gmail.com>
2018-11-14 16:45:07 +1100

    write and document

--- R/readAll.R
+++ R/readAll.R
@@ -1,13 +1,40 @@
+#' Read in & combine
+#' 
+#' Read in all data output by Cell Profiler (e.g. for one cell line/experiment) 
+#' and combine (bind rows) together into on data.table.
+#' 
+#' @param paths Character vector of folder paths the Cell Profiler data is in.
+#'     Function will use \code{list.files} on each folder path and read in
+#'     all files in each folder.
+#' @param sep The field separator character. Values on each line of the file 
+#'     are separated by this character.
+#' 
+#' @importFrom assertthat assert_that
+#' 
 
+#' @export
+readALL <- function(paths, sep = ",") {
+  
+  # check input
+  assert_that(is.character(paths), msg = "Check that paths is a string")
+  
+  assert_that(is.character(sep), length(sep) == 1,
+              msg = "Check that 'sep' is a single string")
+  
+  
+  # list all files in paths
+  all_files <- unlist(sapply(paths, list.files))
 
-# # create vector of file paths for run1 and run2
-# path1 <- list.files("Data/V16D_run_week_20180509/final_spreadsheets", full.names = TRUE)
-# path2 <- list.files("Data/V16D_run_week_20180516/final_spreadsheets", full.names = TRUE)
-# 
-# # combine the 2 vector paths
-# fullpath <- c(path1, path2)
-# 
-# # read in the data and combine together into 1 data.table
-# data_raw <- rbindlist(lapply(fullpath, function(x) fread(
-#   x, sep = ",", header = TRUE)), 
-#   fill = TRUE, use.names = TRUE)
+  # read and combine
+  data_raw <- data.table::rbindlist(lapply(fullpath, 
+                                           function(x) 
+                                             data.table::fread(x, sep = ",", 
+                                                               header = TRUE)), 
+                                    fill = TRUE, use.names = TRUE)
+  # note fill = TRUE fills in missing columns with NAs and use.names checks
+  # column names when binding.
+  
+  return(data_raw)
+  
+}
+
