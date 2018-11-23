b508252ff002cbd0d2b84851ddc4cbeaa4b05b5d
lucyleeow <jliu176@gmail.com>
2018-11-23 13:08:26 +1100

    add calc_negCtrl fun

--- R/plot_negCtrl.R
+++ R/plot_negCtrl.R
@@ -1,46 +1,58 @@
-#' Plot distribution of negative control wells
+#' Assess negative control wells
 #' 
-#' Create boxplot of the negative control wells for each plate for a given 
-#' feature.
-#' 
-#' @return Creates a plot in the current graphics device.
+#' \itemize{
+#'     \item \code{plot_negCtrl} Create boxplot of the negative control wells 
+#'     for each plate for a given feature, in the current graphics device.
+#'     \item \code{calc_negCtrl} will calculate the mean, standard deviation
+#'     and coefficient of variation for the negative control wells for each 
+#'     plate given a feature.
+#' }
 #' 
 #' @param df Dataframe of Cell Profiler data including well annotations.
 #' @param feature Name of the feature column to plot as string.
 #' @param neg_ctrl The term labelling the negative control wells in the 
 #'     annotation (e.g. "DMSO"), as string.
-#' @param annot Name of the column containing the negative control terms
+#' @param annot_col Name of the column containing the negative control terms
 #'     as string.
 #'     
 #' 
 #' @importFrom assertthat assert_that
 #' @importFrom magrittr %>%
+#' @importFrom rlang !!
 #' @import ggplot2
 #' 
+#' @describeIn plot_negCtrl
 #' @export
-plot_negCtrl <- function(df, feature, annot, neg_ctrl){
+plot_negCtrl <- function(df, feature, annot_col, neg_ctrl){
   
   # check inputs
-  assert_that(is.character(feature), length(feature) == 1,
-              msg = "Check that 'feature' is a single string")
+  ## character inputs
+  character_args <- list(feature = feature, annot_col = annot_col,
+                         neg_ctrl = neg_ctrl)
   
-  assert_that(is.character(annot), length(annot) == 1,
-              msg = "Check that 'annot' is a single string")
+  for (i in 1:length(character_args)){
+    
+    msg <- paste("Check that '", names(character_args)[i], 
+                 "' is a single string",
+                 sep = "")
+    
+    assert_that(is.character(character_args[[i]]), 
+                length(character_args[[i]]) == 1,
+                msg = msg)
+    
+  }
   
-  assert_that(is.character(neg_ctrl), length(neg_ctrl) == 1,
-              msg = "Check that 'neg_ctrl' is a single string")
-  
-  assert_that(sum(feature %in% colnames(df)) == 1,
+  assert_that(sum(colnames(df) %in% annot_col) == 1,
               msg = "Check that your 'feature' is a column that exists in 'df'")
   
-  assert_that(sum("Metadata_Barcode" %in% colnames(df)) == 1,
+  assert_that(sum(colnames(df) %in% "Metadata_Barcode") == 1,
               msg = "Check that there is a column called 'Metadata_Barcode' in your 'df'")
   
-  assert_that(sum(annot %in% colnames(df)) == 1,
-              msg = "Check that 'annot' is a column that exists in 'df'")
+  assert_that(sum(colnames(df) %in% annot_col) == 1,
+              msg = "Check that 'annot_col' is a column that exists in 'df'")
   
-  assert_that(sum(df[[annot]] == neg_ctrl) >= 1,
-              msg = "Check taht 'neg_ctrl' is correct")
+  assert_that(sum(df[[annot_col]] == neg_ctrl) >= 1,
+              msg = "Check that 'neg_ctrl' is correct")
   
   
   # make plot
@@ -48,7 +60,7 @@ plot_negCtrl <- function(df, feature, annot, neg_ctrl){
                  sep = "")
   
   return(
-  df[df[,annot] == neg_ctrl,] %>%
+  df[df[,annot_col] == neg_ctrl,] %>%
     ggplot(aes_string(y = feature, x = "Metadata_Barcode")) + 
     geom_boxplot() + 
     labs(title = title) +
@@ -56,3 +68,50 @@ plot_negCtrl <- function(df, feature, annot, neg_ctrl){
   )
     
 }
+
+
+
+#' @describeIn plot_negCtrl
+#' @export
+calc_negCtrl <- function(df, feature, annot_col, neg_ctrl){
+  
+  # check inputs
+  ## character inputs
+  character_args <- list(feature = feature, annot_col = annot_col,
+                         neg_ctrl = neg_ctrl)
+  
+  for (i in 1:length(character_args)){
+    
+    msg <- paste("Check that '", names(character_args)[i], 
+                 "' is a single string",
+                 sep = "")
+    
+    assert_that(is.character(character_args[[i]]), 
+                length(character_args[[i]]) == 1,
+                msg = msg)
+    
+  }
+  
+  assert_that(sum(colnames(df) %in% annot_col) == 1,
+              msg = "Check that your 'feature' is a column that exists in 'df'")
+  
+  assert_that(sum(colnames(df) %in% "Metadata_Barcode") == 1,
+              msg = "Check that there is a column called 'Metadata_Barcode' in your 'df'")
+  
+  assert_that(sum(colnames(df) %in% annot_col) == 1,
+              msg = "Check that 'annot_col' is a column that exists in 'df'")
+  
+  assert_that(sum(df[[annot_col]] == neg_ctrl) >= 1,
+              msg = "Check that 'neg_ctrl' is correct")
+  
+  
+  df %>% 
+    filter(!! as.name(annot_col) == "DMSO") %>%
+    group_by(Metadata_Barcode) %>%
+    summarise(Mean = mean(!! as.name(feature)),
+              SD = sd(!! as.name(feature)),
+              CV = Mean/SD) %>%
+    arrange(desc(CV))
+  
+}
+
