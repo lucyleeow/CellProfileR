497e13cb782cc11dd20e89c5f5c56c0dad9a8157
lucyleeow <jliu176@gmail.com>
2019-01-04 11:32:26 +1100

    add scale functionality to plot_plls

--- R/plot_plls.R
+++ R/plot_plls.R
@@ -6,13 +6,16 @@
 #' @param df Dataframe containing a 'ImageQuality_PowerLogLogSlope' column
 #' @param channel Name of the channel to be plotted, exactly as written in
 #'     Cell Profiler output, as string.
+#' @param scale Single logical. If TRUE the scale of all plots will be uniform.
+#'     The x-axis limits will be set to the min and max of the powerloglog slope 
+#'     for all plates.
 #'     
 #' @importFrom assertthat assert_that
 #' @import ggplot2
 #' @importFrom magrittr %>% 
 #' 
 #' @export
-plot_plls <- function(df, channel) {
+plot_plls <- function(df, channel, scale) {
   
   columnName <- paste("ImageQuality_PowerLogLogSlope_Orig",
                       channel, sep = "")
@@ -21,15 +24,36 @@ plot_plls <- function(df, channel) {
   assert_that(columnName %in% colnames(df),
               msg = "Make sure channel exists and is spelt correctly")
   
-  # make plots
-  plots <- df %>%
-    dplyr::group_by(Metadata_Barcode) %>%
-    dplyr::do(plots = ggplot(data = ., 
-                      aes_string(x = columnName)) + 
-         geom_density() +
-         labs(title = .$Metadata_Barcode[1])
-    )
+  assert_that(is.logical(scale), length(scale) == 1,
+              msg = "Check 'scale' is a single logical")
   
+  # make plots
+  if (scale) {
+    
+    min_plls <- min(data_raw[[columnName]])
+    max_plls <- max(data_raw[[columnName]])
+    
+    plots <- df %>%
+      dplyr::group_by(Metadata_Barcode) %>%
+      dplyr::do(plots = ggplot(data = ., aes_string(x = columnName)) + 
+                  geom_density() +
+                  coord_cartesian(xlim = c(min_plls,max_plls)) +
+                  labs(title = .$Metadata_Barcode[1])
+      )
+    
+  } else {
+    
+    plots <- df %>%
+      dplyr::group_by(Metadata_Barcode) %>%
+      dplyr::do(plots = ggplot(data = ., 
+                               aes_string(x = columnName)) + 
+                  geom_density() +
+                  labs(title = .$Metadata_Barcode[1])
+      )
+    
+  }
+  
+  # print plots
   for (i in 1:length(unique(df$Metadata_Barcode))){
     
     print(plots$plots[[i]])
