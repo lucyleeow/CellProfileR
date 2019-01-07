6bdfca64d2e8d6952f45d0ef38a8403dc980b3b8
lucyleeow <jliu176@gmail.com>
2019-01-07 15:12:34 +1100

    update documentation, add print for error checking

--- R/norm_all.R
+++ R/norm_all.R
@@ -16,9 +16,9 @@
 #' }
 #'
 #' @param df Dataframe containing the CellProfiler data to normalise.
-#' @param data_cols Indicies of the data columns of df. All of these columns
-#'     should be of numerice type. All remaining (non 'data_cols') should NOT 
-#'     be of numeric type.
+#' @param data_cols Numeric vector. Indicies of the data columns of df. All of 
+#'     these columns should be of numerice type. All remaining (non 'data_cols')
+#'     should NOT be of numeric type.
 #' @param count_col Name of the column containing the raw cell counts, as 
 #'     string.
 #' @param compound_col Name of the column in 'df' containing the compound 
@@ -37,7 +37,27 @@ norm_all <- function(df, data_cols, count_col = "Count_Cells", compound_col,
                      negCtrl, rescale_type = "robust") {
   
   # check inputs
+  assert_that(is.numeric(data_cols), 
+              msg = "Check 'data_cols' is of numeric type")
   
+  
+  ## character inputs
+  character_args <- list(count_col = count_col, compound_col = compound_col, 
+                         negCtrl = negCtrl, rescale_type = rescale_type)
+  
+  for (i in 1:length(character_args)){
+    
+    msg <- paste("Check that '", names(character_args)[i], 
+                 "' is a single string",
+                 sep = "")
+    
+    assert_that(is.character(character_args[[i]]), 
+                length(character_args[[i]]) == 1,
+                msg = msg)
+    
+  }
+  
+  ## check column data type
   check_numCols <- apply(df[,data_cols], 2, is.numeric)
   
   assert_that(sum(check_numCols) == length(check_numCols),
@@ -56,6 +76,8 @@ norm_all <- function(df, data_cols, count_col = "Count_Cells", compound_col,
     dplyr::do(reshape::rescaler(., type = rescale_type)) %>%
     dplyr::ungroup()
   
+  print("norz_z done")
+  
   ## rename count column
   colnames(norm_z)[colnames(norm_z) == count_col] <- "CP_Zscore_Count_Cells"
   
@@ -66,6 +88,8 @@ norm_all <- function(df, data_cols, count_col = "Count_Cells", compound_col,
              mean(.[[count_col]][.[[compound_col]] == negCtrl], 
                   na.rm = TRUE))) %>%
     dplyr::select(CP_NormToNeg_Count_Cells)
+  
+  print("norm to neg done")
   
   
   # Raw count data
