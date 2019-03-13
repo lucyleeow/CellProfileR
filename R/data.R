118f7fb1b67625e3cd35786bb9d9a2b19bc74766
lucyleeow <jliu176@gmail.com>
2019-03-13 19:30:14 +1100

    update formating of data doc

--- R/data.R
+++ R/data.R
@@ -2,31 +2,17 @@
 #' 
 #' Raw CellProfiler data from imaging cells in a 384 (24 by 16) well plate. 
 #' Cells in each well were exposed to a different compound, left to grow and
-#' the imaged using three stains; CMFDA, DAPI and Phalloidin. CellProfiler was 
-#' then used to extract phenotype data from each image.
+#' then imaged using three stains; CMFDA, DAPI and Phalloidin. CellProfiler was 
+#' then used to extract phenotype data from images taken of each well.
 #' 
-#' @format A data.table object with 1600 rows (800 rows per plate) and 371
-#' variables. Each row provides metatdata and information extracted by 
-#' CellProfiler from one image taken of cells grown in one well. There are
-#' four rows (images) per well. There are two 384 well plates. Only 200 wells 
-#' from each 384 well plate are included to minimise the size of the data set.
-#' A number of columns output by CellProfiler were also removed (including 
-#' 'Mean' and 'StDev' columns) to minimise the size of the data set. Details
-#' of the columns are described below: 
-#' \describe{
-#'     \item{Metadata_Barcode}{Plate ID.}
-#'     \item{Metadata_WellID}{Well ID. The plate is numbered such that the 
-#'       columns go from 1-24 and the rows from A-P.}
-#'     \item{Compound_ID}{The compound ID of the compound the well was exposed
-#'       to. "DMSO" was the negative control compound.}
-#'     \item{Concentration}{The concentration of the compound the well was
-#'       exposed to. Several concentrations of the same compound was used for
-#'       some compounds.}
-#' }
+#' Each row provides metatdata and information extracted by 
+#' CellProfiler from one image taken of cells grown in one well. There were
+#' four rows (images) taken per well. There were two 384 well plates. Only 200 
+#' wells from each 384 well plate were included to minimise the size of the data 
+#' set. A number of columns output by CellProfiler were also removed (including 
+#' 'Mean' and 'StDev' columns) to minimise the size of the data set.
 #' 
-#' The remaining columns were output by CellProfiler. Phenotype columns 
-#' from CellProfiler have the general structure: 
-#' `MeasurementType_Location_Category_SpecificFeatureName` 
+#' Details of how the CellProfiler columns were named are as follows:
 #' \describe{
 #'     \item{MeasurementType}{This tells you the type of data contained in the
 #'       measurement. Most columns are 'Median', which means that the median
@@ -42,10 +28,29 @@
 #'     \item{SpecificFeatureName}{Details about the measurement made.}
 #' }
 #' 
-#' More details about how each column (variable) is named can be found 
-#' \href{http://cellprofiler-manual.s3.amazonaws.com/CellProfiler-3.1.5/help/output_measurements.html}{here}
-#' and details about how each variable is calculated can be found 
-#' \href{http://cellprofiler-manual.s3.amazonaws.com/CellProfiler-3.1.5/modules/measurement.html?highlight=available%20measurements}{here}.
+#' More details about how each column (variable) is named and how each variable 
+#' is calculated can be found in the 
+#' \href{http://cellprofiler-manual.s3.amazonaws.com/CellProfiler-3.1.5/index.html}{CellProfiler documentation}.
+#' 
+#' 
+#' @format A data.table with 1600 rows, 800 rows per plate, and 371 
+#' variables:
+#' \describe{
+#'     \item{Metadata_Barcode}{Plate ID.}
+#'     \item{Metadata_WellID}{Well ID. The plate is numbered such that the 
+#'       columns go from 1-24 and the rows from A-P.}
+#'     \item{Compound_ID}{The compound ID of the compound the well was exposed
+#'       to. "DMSO" was the negative control compound.}
+#'     \item{Concentration}{The concentration of the compound the well was
+#'       exposed to. Several concentrations of the same compound was used for
+#'       some compounds.}
+#'     \item{CellProfiler output}{The remaining columns were output by 
+#'       CellProfiler. Phenotype columns from CellProfiler have the general 
+#'       structure: `MeasurementType_Location_Category_SpecificFeatureName` and
+#'       are described in more detail below.}
+#' }   
+#'       
+#' 
 #' 
 #' 
 "CP_data"
